9961a3deba5e1cd1c605f0f32c09efb68242d628
sim minjeong <newzerto8@naver.com>
2023-07-19 01:15:46 +0900

    edit 기능 추가

--- django-project/myapp/blog/urls.py
+++ django-project/myapp/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.View.as_view(), name='list'),
     path("write/", views.Write.as_view(), name='write'),
     path("detail/<int:pk>/", views.Detail.as_view(), name='detail'),
+    path("detail/<int:pk>/edit/", views.Update.as_view(), name='edit'),
 ]
 
--- django-project/myapp/blog/views.py
+++ django-project/myapp/blog/views.py
@@ -1,6 +1,7 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.views import View
 from .models import Post
 from .forms import PostForm
@@ -27,3 +28,17 @@ class Detail(DetailView):
     model = Post
     template_name ='blog/post_detail.html'
     context_object_name='post'
+
+
+class Update(UpdateView):
+    model = Post
+    template_name='blog/post_edit.html'
+    fields = ['title', 'content']
+    def get_initial(self):
+        initial = super().get_initial()
+        post = self.get_object()
+        initial['title'] = post.title
+        initial['content'] = post.content
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse_lazy('blog:detail', kwargs={'pk':post.pk})
