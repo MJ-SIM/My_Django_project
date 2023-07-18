65b9432df859cafea4214860dd92a99a14524b72
sim minjeong <newzerto8@naver.com>
2023-07-18 11:19:20 +0900

    detail page

--- django-project/myapp/blog/urls.py
+++ django-project/myapp/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path("", views.View.as_view()),
+    path("", views.View.as_view(), name='list'),
+    path("write/", views.Write.as_view(), name='write'),
+    path("detail/<int:pk>/", views.Detail.as_view(), name='detail'),
 ]
 
--- django-project/myapp/blog/views.py
+++ django-project/myapp/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, DetailView
 from django.views import View
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 class View(View):
     def get(self, request):
-        return render(request,'blog/post_list.html')
+        post = Post.objects.all()
+        context = {
+            'posts': post
+        }
+        return render(request,'blog/post_list.html', context)
+    
+
+class Write(CreateView):
+    model = Post #모델
+    form_class = PostForm #어떤 폼을 사용할건지
+    success_url = reverse_lazy('blog:list') #성공할 경우 보내줄 url
+
+
+class Detail(DetailView):
+    model = Post
+    template_name ='blog/post_detail.html'
+    context_object_name='post'
