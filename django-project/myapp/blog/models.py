f283e8ba4b8f52ef9a457a8099d15c2186f10af1
sim minjeong <newzerto8@naver.com>
2023-07-19 10:59:28 +0900

    Comment Model수정

--- django-project/myapp/blog/models.py
+++ django-project/myapp/blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default = '')
     content = models.TextField()
     writer = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
